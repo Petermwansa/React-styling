d6862bcaa6db8d5073062667f79b4a52659e6d37
Petermwansa <mwansapeter800@gmail.com>
2024-04-09 06:26:21 +0300

    Explore the use of CSS module on the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>Peter's Art</h1>
-      <p>Let's create art together.</p>
+      <p className={classes.paragraph}>Let's create art together.</p>
     </header>
   );
 }
